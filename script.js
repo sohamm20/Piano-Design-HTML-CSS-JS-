1461f3eec900bfdc8e4be38248e6483c688b2906
Soham Mirikar <sohammirikar@gmail.com>
2023-01-31 19:02:05 +0530

    Added audio

---
+++ script.js
@@ -0,0 +1,36 @@
+function playC(url) {
+    new Audio("./sounds/C.mp3").play();
+}
+function playCS(url) {
+    new Audio("./sounds/CS.mp3").play();
+}
+function playD(url) {
+    new Audio("./sounds/D.mp3").play();
+}
+function playDS(url) {
+    new Audio("./sounds/DS.mp3").play();
+}
+function playE(url) {
+    new Audio("./sounds/E.mp3").play();
+}
+function playF(url) {
+    new Audio("./sounds/F.mp3").play();
+}
+function playFS(url) {
+    new Audio("./sounds/FS.mp3").play();
+}
+function playG(url) {
+    new Audio("./sounds/G.mp3").play();
+}
+function playGS(url) {
+    new Audio("./sounds/GS.mp3").play();
+}
+function playA(url) {
+    new Audio("./sounds/A.mp3").play();
+}
+function playAS(url) {
+    new Audio("./sounds/AS.mp3").play();
+}
+function playB(url) {
+    new Audio("./sounds/B.mp3").play();
+}
